0175691988bfe14522d191adf837979646425133
Joao Correa <jvi2@sfu.ca>
2023-08-10 03:16:48 -0700

    button fix
    -no exit sound
    -save was pass by accident
    -main has squared buttons

--- phase3 code/button.py
+++ phase3 code/button.py
@@ -20,3 +20,21 @@ def createButton(colour, size, text, hoverColour, pressColour, parent=None):
         f"}}")
     
     return button
+def createButtonSquared(colour, size, text, hoverColour, pressColour, parent=None):
+    button = QPushButton(parent)
+    button.setText(text)
+    button.setFixedSize(size[0],size[1])
+    button.setStyleSheet(
+        f"QPushButton {{"
+        f"   font-size: 24px; padding: 10px;"
+        f"   color: black; background-color: {colour};"
+        f"   border: 3px solid black;"
+        f"}}"
+        f"QPushButton:hover {{"
+        f"   background-color: {hoverColour};"
+        f"}}"
+        f"QPushButton:pressed {{"
+        f"   background-color: {pressColour};"
+        f"}}")
+    
+    return button
